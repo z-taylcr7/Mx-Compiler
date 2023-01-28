bbbb1011b7f7b20ce6aeaa988228695e3411901b
z-taylcr7 <554795808@qq.com>
2023-01-28 23:50:16 +0800

    Take me away plz

--- src/Undecimber/compiler/middleend/opt/MidOptim.java
+++ src/Undecimber/compiler/middleend/opt/MidOptim.java
@@ -21,7 +21,7 @@ public class MidOptim implements ModulePass {
 //            new Glo2Loc().runFunction(function);
             new Mem2Reg().runFunction(function);
         }
-        new Inline().runModule(module);
+//        new Inline().runModule(module);
 
         // re-analyze info for asm
         for (IRFunction function : module.functions) {
