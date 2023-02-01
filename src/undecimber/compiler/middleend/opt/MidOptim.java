06cebcc13607bcd434e7729ce7badd8a46c7c01d
z-taylcr7 <554795808@qq.com>
2023-02-01 21:33:23 +0800

    deprecate inline

--- src/undecimber/compiler/middleend/opt/MidOptim.java
+++ src/undecimber/compiler/middleend/opt/MidOptim.java
@@ -22,9 +22,9 @@ public class MidOptim implements ModulePass {
 
         for (IRFunction function : module.functions) {
             new Glo2Loc().runFunction(function);
-//            new MemToReg().runFunction(function);
+            new MemToReg().runFunction(function);
         }
-//        new Inline().runModule(module);
+        new Inline().runModule(module);
 
         // re-analyze info for asm
         for (IRFunction function : module.functions) {
