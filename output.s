af3456fc1c6eb4a42f44ee616ddf02c67937e510
z-taylcr7 <554795808@qq.com>
2023-02-05 01:02:33 +0800

    不要再卷了辣！！！！

--- output.s
+++ output.s
@@ -1,14 +1,4 @@
 	.text
-	.globl	_glb_init
-	.p2align	1
-	.type	_glb_init,@function
-_glb_init:
-entry.11:
-	j	exit.11
-exit.11:
-	ret
-	.size	_glb_init, .-_glb_init
-                            #function ends.
 	.globl	loop
 	.p2align	1
 	.type	loop,@function
@@ -23,148 +13,146 @@ entry.12:
 	call	_bot_malloc
 	sw	s0, 0(a0)
 	li	t0, 4
-	add	t4, a0, t0
-	mv	t1, zero
+	add	t5, a0, t0
+	mv	t0, zero
 	mv	t2, zero
 	mv	t3, zero
-	li	t5, 0
+	li	t4, 0
 	j	for.cond
-exit.12:
-	lw	s0, 0(sp)
-	lw	ra, 4(sp)
-	addi	sp, sp, 16
-	ret
 for.cond:
-	li	t0, 10
-	blt	t5,t0,for.body
+	li	t1, 10
+	blt	t4,t1,for.body
 	j	for.exit
-for.incr:
-	addi	t5, t5, 1
-	j	for.cond
 for.body:
 	li	t3, 0
 	j	for.cond.1
 for.exit:
 	li	a0, 0
-	li	t5, 0
+	li	t4, 0
 	j	for.cond.4
 for.cond.1:
-	li	t0, 10
-	blt	t3,t0,for.body.1
-	j	for.incr
-for.incr.1:
-	addi	t3, t3, 1
-	j	for.cond.1
+	li	t1, 10
+	blt	t3,t1,for.body.1
+	j	for.exit.1
 for.body.1:
 	li	t2, 0
 	j	for.cond.2
+for.exit.1:
+	addi	t4, t4, 1
+	j	for.cond
 for.cond.2:
-	li	t0, 10
-	blt	t2,t0,for.body.2
-	j	for.incr.1
-for.incr.2:
+	li	t1, 10
+	blt	t2,t1,for.body.2
+	j	for.exit.2
+for.body.2:
+	li	t0, 0
+	j	for.cond.3
+for.exit.2:
+	addi	t3, t3, 1
+	j	for.cond.1
+for.cond.3:
+	li	t1, 10
+	blt	t0,t1,for.body.3
+	j	for.exit.3
+for.body.3:
+	li	t1, 1000
+	mul	t6, t4, t1
+	li	t1, 100
+	mul	t1, t3, t1
+	add	t6, t6, t1
+	li	t1, 10
+	mul	t1, t2, t1
+	add	t1, t6, t1
+	add	t6, t1, t0
+	li	t1, 4
+	mul	t1, t6, t1
+	add	t1, t5, t1
+	add	t6, t4, t0
+	sw	t6, 0(t1)
+	addi	t0, t0, 1
+	j	for.cond.3
+for.exit.3:
 	addi	t2, t2, 1
 	j	for.cond.2
-for.body.2:
-	li	t1, 0
-	j	for.cond.3
-for.cond.3:
-	li	t0, 10
-	blt	t1,t0,for.body.3
-	j	for.incr.2
-for.incr.3:
-	addi	t1, t1, 1
-	j	for.cond.3
-for.body.3:
-	li	t0, 1000
-	mul	t6, t5, t0
-	li	t0, 100
-	mul	t0, t3, t0
-	add	t6, t6, t0
-	li	t0, 10
-	mul	t0, t2, t0
-	add	t0, t6, t0
-	add	t6, t0, t1
-	li	t0, 4
-	mul	t0, t6, t0
-	add	t0, t4, t0
-	add	t6, t5, t1
-	sw	t6, 0(t0)
-	j	for.incr.3
 for.cond.4:
-	li	t0, 10
-	blt	t5,t0,for.body.4
-	j	exit.12
-for.incr.4:
-	addi	t5, t5, 1
-	j	for.cond.4
+	li	t1, 10
+	blt	t4,t1,for.body.4
+	j	for.exit.4
 for.body.4:
 	li	t3, 0
 	j	for.cond.5
+for.exit.4:
+	lw	s0, 0(sp)
+	lw	ra, 4(sp)
+	addi	sp, sp, 16
+	ret
 for.cond.5:
-	li	t0, 10
-	blt	t3,t0,for.body.5
-	j	for.incr.4
-for.incr.5:
-	addi	t3, t3, 1
-	j	for.cond.5
+	li	t1, 10
+	blt	t3,t1,for.body.5
+	j	for.exit.5
 for.body.5:
 	li	t2, 0
 	j	for.cond.6
+for.exit.5:
+	addi	t4, t4, 1
+	j	for.cond.4
 for.cond.6:
-	li	t0, 10
-	blt	t2,t0,for.body.6
-	j	for.incr.5
-for.incr.6:
+	li	t1, 10
+	blt	t2,t1,for.body.6
+	j	for.exit.6
+for.body.6:
+	li	t0, 0
+	j	for.cond.7
+for.exit.6:
+	addi	t3, t3, 1
+	j	for.cond.5
+for.cond.7:
+	li	t1, 10
+	blt	t0,t1,for.body.7
+	j	for.exit.7
+for.body.7:
+	andi	t6, t0, 1
+	li	t1, 1
+	beq	t6,t1,if.true
+	j	if.false
+for.exit.7:
 	addi	t2, t2, 1
 	j	for.cond.6
-for.body.6:
-	li	t1, 0
-	j	for.cond.7
-for.cond.7:
-	li	t0, 10
-	blt	t1,t0,for.body.7
-	j	for.incr.6
-for.incr.7:
-	addi	t1, t1, 1
-	j	for.cond.7
-for.body.7:
-	andi	t6, t1, 1
-	li	t0, 1
-	beq	t6,t0,if.true
-	j	if.false
 if.true:
-	li	t0, 1000
-	mul	t6, t5, t0
-	li	t0, 100
-	mul	t0, t3, t0
-	add	t6, t6, t0
-	li	t0, 10
-	mul	t0, t2, t0
-	add	t0, t6, t0
-	add	t6, t0, t1
-	li	t0, 4
-	mul	t0, t6, t0
-	add	t0, t4, t0
-	lw	t0, 0(t0)
-	add	a0, a0, t0
-	j	for.incr.7
+	li	t1, 1000
+	mul	t6, t4, t1
+	li	t1, 100
+	mul	t1, t3, t1
+	add	t6, t6, t1
+	li	t1, 10
+	mul	t1, t2, t1
+	add	t1, t6, t1
+	add	t6, t1, t0
+	li	t1, 4
+	mul	t1, t6, t1
+	add	t1, t5, t1
+	lw	t1, 0(t1)
+	add	a0, a0, t1
+	j	if.exit
 if.false:
-	li	t0, 1000
-	mul	t6, t5, t0
-	li	t0, 100
-	mul	t0, t3, t0
-	add	t6, t6, t0
-	li	t0, 10
-	mul	t0, t2, t0
-	add	t0, t6, t0
-	add	t6, t0, t1
-	li	t0, 4
-	mul	t0, t6, t0
-	add	t0, t4, t0
-	lw	t0, 0(t0)
-	sub	a0, a0, t0
-	j	for.incr.7
+	li	t1, 1000
+	mul	t6, t4, t1
+	li	t1, 100
+	mul	t1, t3, t1
+	add	t6, t6, t1
+	li	t1, 10
+	mul	t1, t2, t1
+	add	t1, t6, t1
+	add	t6, t1, t0
+	li	t1, 4
+	mul	t1, t6, t1
+	add	t1, t5, t1
+	lw	t1, 0(t1)
+	sub	a0, a0, t1
+	j	if.exit
+if.exit:
+	addi	t0, t0, 1
+	j	for.cond.7
 	.size	loop, .-loop
                             #function ends.
 	.globl	compute
@@ -182,7 +170,15 @@ entry.13:
 	li	s1, 0
 	li	s2, 0
 	j	wh.cond
-exit.13:
+wh.cond:
+	bge	s0,s2,wh.body
+	j	wh.exit
+wh.body:
+	call	loop
+	li	t0, 5000
+	blt	t0,a0,if.true.1
+	j	if.exit.1
+wh.exit:
 	mv	a0, s1
 	lw	s0, 8(sp)
 	lw	s1, 4(sp)
@@ -190,14 +186,6 @@ exit.13:
 	lw	ra, 12(sp)
 	addi	sp, sp, 16
 	ret
-wh.cond:
-	bge	s0,s2,wh.body
-	j	exit.13
-wh.body:
-	call	loop
-	li	t0, 5000
-	blt	t0,a0,if.true.1
-	j	if.exit.1
 if.true.1:
 	li	t0, 5000
 	sub	a0, a0, t0
@@ -225,12 +213,9 @@ main:
 entry.14:
 	addi	sp, sp, -16
 	sw	ra, 0(sp)
-	call	_glb_init
 	li	a0, 20
 	call	compute
 	call	printInt
-	j	exit.14
-exit.14:
 	li	a0, 0
 	lw	ra, 0(sp)
 	addi	sp, sp, 16
--- src/undecimber/compiler/middleend/opt/MidOptim.java
+++ src/undecimber/compiler/middleend/opt/MidOptim.java
@@ -24,7 +24,7 @@ public class MidOptim implements ModulePass {
             new Mem2Reg().runFunction(function);
         }
 
-//        new Inline().runModule(module);
+        new Inline().runModule(module);
         for (IRFunction function : module.functions) {
 //            new BlockMerger().runFunction(function);
         }
@@ -32,7 +32,7 @@ public class MidOptim implements ModulePass {
         // re-analyze info for asm
         for (IRFunction function : module.functions) {
             new SSADestructor().runFunction(function);
-//            new BlockMerger().runFunction(function);
+            new BlockMerger().runFunction(function);
             new LoopAnalyzer().runFunction(function);
             new PrintOptimizer().runFunction(function);
         }
